44875905e27e2578b3c920bb46e741aca03dd366
highreso <miura@highreso.jp>
2020-12-23 09:48:30 +0000

    rev some

---
+++ hello.f90
@@ -0,0 +1,4 @@
+program HelloWorld
+    ! '!'マークでコメントが書けます。「Hello, World!」はやっぱりやめました。滑った？
+    print *, 'Merry Christmas!' 
+end program HelloWorld
--- input.f90
+++ input.f90
@@ -1,8 +1,8 @@
 program input
-    implicit none
-    integer n, m
+    implicit none  ! ルーチン内で変数を用いる際の「おまじない」です
+    integer n, m  ! 変数を定義します。解説は次節で
 
-    ! n, mの入力をキーボードから受け付ける
+    ! n, mの入力をキーボードから受け付ける。整数
     read *, n, m
 
     print *, n, m
